b238289dd1281d28fff088e84c1cbdaa07f70924
Layer8Err <dwight.brenner@gmail.com>
2019-06-22 23:30:50 -0400

    Fix for user permissions

--- install_docker_ce.sh
+++ install_docker_ce.sh
@@ -8,6 +8,12 @@ sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install docker-ce
+
+# Allow $USER to run Docker commands without sudo
+sudo usermod -aG docker $USER
+sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+sudo chown g+rwx "$HOME/.docker" -R
+
 sudo service docker start
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
